1888a0c55a9d630d316cc058f5ad7792b01ac5be
Luca Deri <deri@ntop.org>
2018-08-17 14:27:11 +0200

    Merge of https://github.com/ntop/n2n/pull/26

--- edge_utils.c
+++ edge_utils.c
@@ -17,6 +17,7 @@
  */
 
 #include "n2n.h"
+#include "lzoconf.h"
 
 #ifdef __ANDROID_NDK__
 #include "android/edge_android.h"
@@ -920,8 +921,9 @@ static void readFromMgmtSocket(n2n_edge_t * eee, int * keep_running) {
 		      (unsigned int)peer_list_size(eee->known_peers));
 
   msg_len += snprintf((char *)(udp_buf+msg_len), (N2N_PKT_BUF_SIZE-msg_len),
-		      "last   super:%lu(%ld sec ago) p2p:%lu(%ld sec ago)\n",
-		      eee->last_sup, (now-eee->last_sup), eee->last_p2p, (now-eee->last_p2p));
+		      "last   super:%lu(%ld sec ago) p2p:%lu(%lD sec ago)\n",
+		      eee->last_sup, (now-eee->last_sup), eee->last_p2p,
+		      (now-eee->last_p2p));
 
   traceEvent(TRACE_DEBUG, "mgmt status sending: %s", udp_buf);
 
@@ -1629,7 +1631,13 @@ const char *random_device_mac(void)
             mac[i] = ':';
             continue;
         }
+#ifdef WIN32
+#define random() rand()
+#endif
         mac[i] = key[random() % sizeof(key)];
+#ifdef WIN32
+#undef random()
+#endif
     }
     mac[sizeof(mac) - 1] = '\0';
     return mac;
