77392f8e53418832b6c8c4f7e81c3243c0749f12
emanuele-f <black.silver@hotmail.it>
2019-03-01 20:34:44 +0100

    Add check for SO_REUSEPORT
    Fixes #51

--- edge_utils.c
+++ edge_utils.c
@@ -1747,8 +1747,8 @@ int quick_edge_init(char *device_name, char *community_name,
     
     /* allow multiple sockets to use the same PORT number */
     setsockopt(eee.udp_multicast_sock, SOL_SOCKET, SO_REUSEADDR, &enable_reuse, sizeof(enable_reuse));
-#ifndef WIN32 /* no SO_REUSEPORT in Windows */
-	  setsockopt(eee.udp_multicast_sock, SOL_SOCKET, SO_REUSEPORT, &enable_reuse, sizeof(enable_reuse));
+#ifdef SO_REUSEPORT /* no SO_REUSEPORT in Windows / old linux versions */
+    setsockopt(eee.udp_multicast_sock, SOL_SOCKET, SO_REUSEPORT, &enable_reuse, sizeof(enable_reuse));
 #endif
 
     mreq.imr_multiaddr.s_addr = inet_addr(N2N_MULTICAST_GROUP);
