2148f7ceaff8bf16e1c8dd2a90ed39b767c41585
Logan008 <you@example.com>
2019-07-30 20:47:29 +0200

    added xorshift128+ random number generator including auto reseed

--- random_numbers.c
+++ random_numbers.c
@@ -4,11 +4,18 @@
    on par with C's rand().
  */
 
+
+
 #include <stdint.h>
 #ifdef __GNUC__
 #include <sys/time.h>
 #endif
 #include <time.h>
+#ifdef __linux__
+    #include <sys/syscall.h>
+    #define GRND_NONBLOCK	1
+#endif
+#include "n2n.h"
 #include "random_numbers.h"
 
 struct rn_generator_state_t {
@@ -22,6 +29,8 @@ static struct rn_generator_state_t rn_current_state
 			       = { .a    = 0x9E3779B97F4A7C15,
 			           .b    = 0xBF58476D1CE4E5B9
 };
+
+static uint64_t reseed_counter = 0;
 
 /* taken from benchmark.c */
 #if defined(WIN32) && !defined(__GNUC__)
@@ -47,29 +56,41 @@ static int gettimeofday(struct timeval *tp, void *tzp)
 #endif
 
 static int32_t set_seed (uint64_t seed) {
-    /* shift a --> b , so
-       call this function 2 times to enter full 128 bit seed */
+    /* shift a --> b */
     rn_current_state.b = rn_current_state.a;
     /* for xorshift128p, an all zero state must be avoided
        so, we generally do not allow 0's */
     if (seed != 0) {
 	rn_current_state.a = seed;
+	reseed_counter = 0;
     }
     else {
 	/* otherwise, entropy seed */
 	/* use whatever it was before, no wiping */
 	rn_current_state.a ^= 0xBF58476D1CE4E5B9;
-	/* time(NULL) = currenctUTC in seconds;
-	   clock() = clock_ticks since program start */
-	rn_current_state.a ^= (uint64_t)time(NULL) * (uint64_t)clock();
+	/* time(NULL) = current UTC in seconds */
+	rn_current_state.a ^= (uint64_t)time(NULL) << 32;
+	/* clock() = clock_ticks since program start */
+	rn_current_state.a ^= (uint64_t)clock() * 65537;
 	/* rv_usec = second fraction of current time */
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	rn_current_state.a ^= (uint64_t)tv.tv_usec << 16;
+#ifdef SYS_getrandom
+	uint64_t dev_urandom;
+	if ( syscall(SYS_getrandom, &dev_urandom, sizeof(dev_urandom), GRND_NONBLOCK) == sizeof(dev_urandom) ) {
+	    rn_current_state.a ^= dev_urandom;
+	    traceEvent (TRACE_DEBUG, "random seed: successfully called getrandom syscall");
+	}
+#endif
     }
     /* stabilize in case of weak seed with only a few bits set */
     for (int i = 0; i < 32; i++)
 	random_number_64();
+    /* set the reseed_counter to a value between 1 and 2^18
+       which roughly is the number of packets per two minutes
+       on a 1 Gbit line used to full capacity */
+    reseed_counter = (random_number_64() & 0x3ffff) + 1;
     return (0);
 }
 
@@ -78,6 +99,13 @@ static int32_t set_seed (uint64_t seed) {
    and thus is considered public domain.
  */
 static uint64_t xorshift128p_64 () {
+    if (reseed_counter > 0) {
+	reseed_counter--;
+	if (reseed_counter == 0) {
+	    set_seed (0);
+    	    traceEvent (TRACE_DEBUG, "random seed: successfully reseeded");
+	}
+    }
     uint64_t t =  rn_current_state.a;
     uint64_t const s = rn_current_state.b;
     rn_current_state.a = s;
@@ -89,6 +117,10 @@ static uint64_t xorshift128p_64 () {
 }
 
 static uint32_t xorshift128p_32 () {
+    if (reseed_counter > 0) {
+	reseed_counter--;
+	if (reseed_counter == 0) set_seed (0);
+    }
     uint64_t t = rn_current_state.a;
     uint64_t const s = rn_current_state.b;
     rn_current_state.a = s;
@@ -114,5 +146,9 @@ uint64_t random_number_64 () {
 }
 
 uint32_t random_number_32 () {
+    if (reseed_counter > 0) {
+	reseed_counter--;
+	if (reseed_counter == 0) random_number_seed (0);
+    }
     return (xorshift128p_32 ());
 }
