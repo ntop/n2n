e28ec42ac22e5e7d41190e6b5d840496ccbf2b42
Logan007 <you@example.com>
2020-06-03 21:57:49 +0545

    added SSE support

--- speck.h
+++ speck.h
@@ -1,9 +1,20 @@
 
 #define u64 uint64_t
 
+#ifdef __SSE4_2__
+ #include <immintrin.h>
+ #define u128 __m128i
+typedef struct {
+        u128 rk[34];
+        u64 key[34];
+} speck_context_t;
+#else
+ typedef u64 speck_context_t [34];
+#endif
+
 int speck_ctr (unsigned char *out, const unsigned char *in,
 	       unsigned long long inlen,
                const unsigned char *n,
-               u64 rk[]);
+               speck_context_t ctx);
 
-int speck_expand_key (const unsigned char *k, u64 rk[]);
+int speck_expand_key (const unsigned char *k, speck_context_t ctx);
