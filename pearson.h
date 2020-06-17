9951a917c70139ddb1e978b6e1e87502b10dcf24
Luca Deri <deri@ntop.org>
2020-06-17 06:54:02 +0200

    Updated (C)

--- pearson.h
+++ pearson.h
@@ -1,1 +1,19 @@
+/**
+ * (C) 2007-20 - ntop.org and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not see see <http://www.gnu.org/licenses/>
+ *
+ */
+
 void pearson_hash_256 (uint8_t *out, const uint8_t *in, size_t len);
