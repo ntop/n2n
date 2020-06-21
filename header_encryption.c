632c3e68e862ef35ea7c546a16b8ab759bfbf25d
Logan007 <you@example.com>
2020-06-21 18:39:11 +0545

    polished header encryption interface

--- header_encryption.c
+++ header_encryption.c
@@ -43,11 +43,13 @@ uint32_t packet_header_decrypt (uint8_t packet[], uint16_t packet_len,
   uint32_t magic = 0x6E326E00; // ="n2n_"
   uint32_t test_magic;
   // check for magic bytes and reasonable value in header len field
+  // so, as a first step, decrypt 4 bytes only starting at byte 12
   speck_he ((uint8_t*)&test_magic, &packet[12], 4, iv, (speck_context_t*)ctx);
   test_magic = be32toh (test_magic);
   if ( (((test_magic >> 8) <<  8) == magic)	// check the thre uppermost bytes
        && (((uint8_t)test_magic) <= packet_len) // lowest 8 bit of test_magic are header_len
        ) {
+    // decrypt the complete header
     speck_he (&packet[12], &packet[12], (uint8_t)(test_magic) - 12, iv, (speck_context_t*)ctx);
     // restore original packet order
     memcpy (&packet[0], &packet[16], 4);
@@ -58,59 +60,6 @@ uint32_t packet_header_decrypt (uint8_t packet[], uint16_t packet_len,
 }
 
 
-int8_t packet_header_decrypt_if_required (uint8_t packet[], uint16_t packet_len,
-					  struct sn_community *communities) {
-
-  struct sn_community *c, *tmp;
-
-  if (packet_len < 20)
-    return (-1);
-
-  // first, check if header is unenrypted to put it into the fast-lane then
-
-  // the following check is around 99.99962 percent reliable
-  // it heavily relies on the structure of packet's common part
-  // changes to wire.c:encode/decode_common need to go together with this code
-  if ( (packet[19] == (uint8_t)0x00)	// null terminated community name
-       && (packet[00] == N2N_PKT_VERSION)	// correct packet version
-       && ((be16toh (*(uint16_t*)&(packet[02])) & N2N_FLAGS_TYPE_MASK ) <= MSG_TYPE_MAX_TYPE  ) // message type
-       && ( be16toh (*(uint16_t*)&(packet[02])) < N2N_FLAGS_OPTIONS)	// flags
-       ) {
-
-    // most probably unencrypted
-
-    // make sure, no downgrading happens here and no unencrypted packets can be
-    // injected in a community which definitely deals with encrypted headers
-    HASH_FIND_COMMUNITY(communities, (char *)&packet[04], c);
-    if (c)
-      if (c->header_encryption == HEADER_ENCRYPTION_ENABLED)
-	return (-2);
-    // set 'no encryption' in case it is not set yet
-    c->header_encryption = HEADER_ENCRYPTION_NONE;
-    c->header_encryption_ctx = NULL;
-    return (HEADER_ENCRYPTION_NONE);
-  } else {
-
-    // most probably encrypted
-    // cycle through the known communities (as keys) to eventually decrypt
-    uint32_t ret;
-    HASH_ITER (hh, communities, c, tmp) {
-      // skip the definitely unencrypted communities
-      if (c->header_encryption == HEADER_ENCRYPTION_NONE)
-	continue;
-      if ( (ret = packet_header_decrypt (packet, packet_len, c->community, c->header_encryption_ctx)) ) {
-	// set 'encrypted' in case it is not set yet
-        c->header_encryption = HEADER_ENCRYPTION_ENABLED;
-	// no need to test further communities
-	return (HEADER_ENCRYPTION_ENABLED);
-      }
-    }
-    // no matching key/community
-    return (-3);
-  }
-}
-
-
 int32_t packet_header_encrypt (uint8_t packet[], uint8_t header_len, he_context_t * ctx) {
 
   uint8_t iv[16];
@@ -118,8 +67,10 @@ int32_t packet_header_encrypt (uint8_t packet[], uint8_t header_len, he_context_
   uint64_t *iv64 = (uint64_t*)&iv;
   const uint32_t magic = 0x6E326E21; // = ASCII "n2n!"
 
-  if (header_len < 20)
+  if (header_len < 20) {
+    traceEvent(TRACE_DEBUG, "packet_header_encrypt dropped a packet too short to be valid.");
     return (-1);
+  }
 
   memcpy (&packet[16], &packet[00], 4);
 
