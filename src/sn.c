c94005ecb608773035b2d05fa156411b831a70e3
francesco_carli <62562180+fcarli3@users.noreply.github.com>
2020-09-23 16:56:27 +0200

    Add comm_init() function

--- src/sn.c
+++ src/sn.c
@@ -89,12 +89,9 @@ static int load_allowed_sn_community(n2n_sn_t *sss, char *path) {
       }
     }
 
-    s = (struct sn_community*)calloc(1,sizeof(struct sn_community));
+    comm_init(s,cmn_str);
 
     if(s != NULL) {
-      strncpy((char*)s->community, cmn_str, N2N_COMMUNITY_SIZE-1);
-      s->community[N2N_COMMUNITY_SIZE-1] = '\0';
-      s->is_federation = IS_NO_FEDERATION;
       /* loaded from file, this community is unpurgeable */
       s->purgeable = COMMUNITY_UNPURGEABLE;
       /* we do not know if header encryption is used in this community,
