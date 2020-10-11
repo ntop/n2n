ntop/n2n

commit c9eedd68f06cfe76fda6a817f960ab8fa61f5509

Initialize the federation of supernodes (#460)
* Add supernode2sock() and add_sn_to_federation_by_mac_or_sock()

* Update sn_utils.c

* Update sn.c and sn_utils.c

* Update REG_SUPER_ACK payload

* Update add_sn_to_federation_by_mac_or_sock()

include/n2n.h
@@ -190,23 +190,25 @@ typedef char macstr_t[N2N_MACSTR_SIZE];
 typedef char dec_ip_str_t[N2N_NETMASK_STR_SIZE];
 typedef char dec_ip_bit_str_t[N2N_NETMASK_STR_SIZE + 4];
 
+typedef struct speck_context_t he_context_t;
+typedef char n2n_sn_name_t[N2N_EDGE_SN_HOST_SIZE];
+
 
 struct peer_info {
   n2n_mac_t        mac_addr;
   n2n_ip_subnet_t  dev_addr;
   n2n_sock_t       sock;
   int              timeout;
+  uint8_t          purgeable;
   time_t           last_seen;
   time_t           last_p2p;
   time_t           last_sent_query;
   uint64_t         last_valid_time_stamp;
+  char             *ip_addr;
 
   UT_hash_handle   hh; /* makes this structure hashable */
 };
 
-
-typedef struct speck_context_t he_context_t;
-typedef char n2n_sn_name_t[N2N_EDGE_SN_HOST_SIZE];
 
 typedef struct n2n_route {
   in_addr_t        net_addr;
@@ -396,7 +398,7 @@ typedef struct n2n_sn
   int lock_communities; /* If true, only loaded and matching communities can be used. */
   struct sn_community *communities;
   struct sn_community_regular_expression *rules;
-  char federation[N2N_COMMUNITY_SIZE];
+  struct sn_community *federation;
 } n2n_sn_t;
 
 /* ************************************** */
@@ -443,6 +445,7 @@ uint32_t bitlen2mask(uint8_t bitlen);
 uint8_t mask2bitlen(uint32_t mask);
 char* macaddr_str(macstr_t buf, const n2n_mac_t mac);
 int str2mac( uint8_t * outmac /* 6 bytes */, const char * s );
+int supernode2sock(n2n_sock_t * sn, const n2n_sn_name_t addrIn);
 uint8_t is_multi_broadcast(const uint8_t * dest_mac);
 char* msg_type2str(uint16_t msg_type);
 void hexdump(const uint8_t * buf, size_t len);
@@ -497,6 +500,7 @@ int quick_edge_init(char *device_name, char *community_name,
 int comm_init(struct sn_community *comm, char *cmn);
 int sn_init(n2n_sn_t *sss);
 void sn_term(n2n_sn_t *sss);
+struct peer_info* add_sn_to_federation_by_mac_or_sock(n2n_sn_t *sss, n2n_sock_t *sock, n2n_mac_t *mac);
 int run_sn_loop(n2n_sn_t *sss, int *keep_running);
 int assign_one_ip_subnet(n2n_sn_t *sss, struct sn_community *comm);
 const char* compression_str(uint8_t cmpr);

include/n2n_wire.h
@@ -169,8 +169,6 @@ typedef struct n2n_REGISTER_SUPER_ACK {
   uint8_t              num_sn;         /**< Number of supernodes that were send
 					* even if we cannot store them all. If
 					* non-zero then sn_bak is valid. */
-  n2n_sock_t           sn_bak;         /**< Socket of the first backup supernode */
-  n2n_mac_t	       mac_addr;
 } n2n_REGISTER_SUPER_ACK_t;
 
 

src/sn.c
@@ -24,6 +24,7 @@
 #define HASH_FIND_COMMUNITY(head, name, out) HASH_FIND_STR(head, name, out)
 
 static n2n_sn_t sss_node;
+static const n2n_mac_t null_mac = {0, 0, 0, 0, 0, 0};
 
 /** Load the list of allowed communities. Existing/previous ones will be removed
  *
@@ -176,8 +177,9 @@ static void help() {
 	 "or\n"
 	 );
   printf("supernode ");
-  printf("-l <local port> ");
+  printf("-p <local port> ");
   printf("-c <path> ");
+  printf("-l <supernode:port> ");
 #if defined(N2N_HAVE_DAEMON)
   printf("[-f] ");
 #endif
@@ -191,8 +193,9 @@ static void help() {
   printf("[-v] ");
   printf("\n\n");
 
-  printf("-l <port>         | Set UDP main listen port to <port>\n");
+  printf("-p <port>         | Set UDP main listen port to <port>\n");
   printf("-c <path>         | File containing the allowed communities.\n");
+  printf("-l <sn:port>      | Supernode IP:port.\n");
 #if defined(N2N_HAVE_DAEMON)
   printf("-f                | Run in foreground.\n");
 #endif /* #if defined(N2N_HAVE_DAEMON) */
@@ -219,13 +222,52 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
   //traceEvent(TRACE_NORMAL, "Option %c = %s", optkey, _optarg ? _optarg : "");
 
   switch (optkey) {
-  case 'l': /* local-port */
+  case 'p': /* local-port */
     sss->lport = atoi(_optarg);
     break;
 
   case 't': /* mgmt-port */
     sss->mport = atoi(_optarg);
     break;
+
+  case 'l': { /* supernode:port */
+    n2n_sock_t *socket;
+  	struct peer_info *anchor_sn;
+    size_t length;
+    int rv;
+
+    length = strlen(_optarg);
+    if(length >= N2N_EDGE_SN_HOST_SIZE) {
+      traceEvent(TRACE_WARNING, "Size of -l argument too long: %zu. Maximum size is %d",length,N2N_EDGE_SN_HOST_SIZE);
+      break;
+    }
+
+    if(sss->federation != NULL) {
+  		socket = (n2n_sock_t *)calloc(1,sizeof(n2n_sock_t));
+
+      anchor_sn = add_sn_to_federation_by_mac_or_sock(sss,socket, (n2n_mac_t*) null_mac);
+
+      if(anchor_sn != NULL){
+        anchor_sn->ip_addr = calloc(1,N2N_EDGE_SN_HOST_SIZE);
+        if(anchor_sn->ip_addr){
+          strncpy(anchor_sn->ip_addr,_optarg,N2N_EDGE_SN_HOST_SIZE-1);
+          rv = supernode2sock(socket,_optarg);
+
+          if(rv != 0){
+            traceEvent(TRACE_WARNING, "Invalid socket");
+            break;
+          }
+
+        	memcpy(&(anchor_sn->sock), socket, sizeof(n2n_sock_t));
+          memcpy(&(anchor_sn->mac_addr),null_mac,sizeof(n2n_mac_t));
+          anchor_sn->purgeable = SN_UNPURGEABLE;
+          anchor_sn->last_valid_time_stamp = initial_time_stamp();
+        }
+      }
+    }
+    
+    break;
+  }
 
   case 'a': {
     dec_ip_str_t ip_min_str = {'\0'};
@@ -280,15 +322,12 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
 #endif
 
   case 'F': { /* federation name */
-    struct sn_community *fed;
 
-    HASH_FIND_COMMUNITY(sss->communities, FEDERATION_NAME, fed);
-
-    if(fed != NULL){
-      snprintf(fed->community,N2N_COMMUNITY_SIZE-1,"*%s",_optarg);
-      strncpy(sss->federation, fed->community, N2N_COMMUNITY_SIZE-1);
-      sss->federation[N2N_COMMUNITY_SIZE-1] = '\0';
+    if(sss->federation->community != NULL){
+      snprintf(sss->federation->community,N2N_COMMUNITY_SIZE-1,"*%s",_optarg);
+      sss->federation->community[N2N_COMMUNITY_SIZE-1] = '\0';
     }
+
     break;
   }
 
@@ -327,7 +366,7 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
 static const struct option long_options[] = {
 					     {"communities", required_argument, NULL, 'c'},
 					     {"foreground",  no_argument,       NULL, 'f'},
-					     {"local-port",  required_argument, NULL, 'l'},
+					     {"local-port",  required_argument, NULL, 'p'},
 					     {"mgmt-port",   required_argument, NULL, 't'},
 					     {"autoip",      required_argument, NULL, 'a'},
 					     {"help",        no_argument,       NULL, 'h'},
@@ -341,7 +380,7 @@ static const struct option long_options[] = {
 static int loadFromCLI(int argc, char * const argv[], n2n_sn_t *sss) {
   u_char c;
 
-  while((c = getopt_long(argc, argv, "fl:u:g:t:a:c:F:m:vh",
+  while((c = getopt_long(argc, argv, "fp:l:u:g:t:a:c:F:m:vh",
 			 long_options, NULL)) != '?') {
     if(c == 255) break;
     setOption(c, optarg, sss);
@@ -437,26 +476,15 @@ static int loadFromFile(const char *path, n2n_sn_t *sss) {
 
 /* Add the federation to the communities list of a supernode */
 static int add_federation_to_communities(n2n_sn_t *sss){
-  struct sn_community *fed;
   uint32_t  num_communities = 0;
 
-  fed = (struct sn_community *)calloc(1,sizeof(struct sn_community));
-  comm_init(fed,sss->federation);
-
-  if(fed != NULL) {
-    /* enable the flag for federation */
-    fed->is_federation = IS_FEDERATION;
-    fed->purgeable = COMMUNITY_UNPURGEABLE;
-    /* header encryption enabled by default */
-    fed->header_encryption = HEADER_ENCRYPTION_ENABLED;
-    /*setup the encryption key */
-    packet_header_setup_key(fed->community, &(fed->header_encryption_ctx), &(fed->header_iv_ctx));
-    HASH_ADD_STR(sss->communities, community, fed);
+  if(sss->federation != NULL) {
+    HASH_ADD_STR(sss->communities, community, sss->federation);
 
     num_communities = HASH_COUNT(sss->communities);
 
     traceEvent(TRACE_INFO, "Added federation '%s' to the list of communities [total: %u]",
-  		 (char*)fed->community, num_communities);
+  		 (char*)sss->federation->community, num_communities);
   }
 
   return 0;
@@ -615,5 +643,3 @@ int main(int argc, char * const argv[]) {
   keep_running = 1;
   return run_sn_loop(&sss_node, &keep_running);
 }
-
-

commit 9aebb382ee965cc536a6501bc0258a90ea1bc4f8

Fixes minor issues introduced by #460

include/n2n.h
@@ -500,6 +500,7 @@ int quick_edge_init(char *device_name, char *community_name,
 int comm_init(struct sn_community *comm, char *cmn);
 int sn_init(n2n_sn_t *sss);
 void sn_term(n2n_sn_t *sss);
+int supernode2sock(n2n_sock_t * sn, const n2n_sn_name_t addrIn);
 struct peer_info* add_sn_to_federation_by_mac_or_sock(n2n_sn_t *sss, n2n_sock_t *sock, n2n_mac_t *mac);
 int run_sn_loop(n2n_sn_t *sss, int *keep_running);
 int assign_one_ip_subnet(n2n_sn_t *sss, struct sn_community *comm);

include/n2n_define.h
@@ -59,7 +59,7 @@
 
 /* parameters for replay protection */
 #define TIME_STAMP_FRAME	0x0000001000000000LL /* clocks of different computers are allowed +/- 16 seconds to be off */
-#define TIME_STAMP_JITTER	0x0000000027100000LL /* we allow a packet to arrive 160 ms (== 0x27100 us) before another 
+#define TIME_STAMP_JITTER	0x0000000027100000LL /* we allow a packet to arrive 160 ms (== 0x27100 us) before another
                                                       * set to 0x0000000000000000LL if increasing (or equal) time stamps allowed only */
 #define TIME_STAMP_ALLOW_JITTER                    1 /* constant for allowing or... */
 #define TIME_STAMP_NO_JITTER                       0 /* not allowing jitter to be considered */
@@ -82,8 +82,8 @@ enum federation{IS_NO_FEDERATION = 0,IS_FEDERATION = 1};
 #define COMMUNITY_UNPURGEABLE		0
 #define COMMUNITY_PURGEABLE		1
 
-#define SN_UNPURGEABLE		        0  /* FIX fcarli3 */
-#define SN_PURGEABLE		        1  /* FIX fcarli3 */
+/* (un)purgeable supernode indicator */
+enum sn_purge{SN_UNPURGEABLE = 0, SN_PURGEABLE = 1};
 
 /* Header encryption indicators */
 #define HEADER_ENCRYPTION_UNKNOWN       0
@@ -139,4 +139,3 @@ enum federation{IS_NO_FEDERATION = 0,IS_FEDERATION = 1};
 #ifndef min
 #define min(a, b) ((a > b) ? b : a)
 #endif
-

include/n2n_wire.h
@@ -304,13 +304,15 @@ int decode_REGISTER_ACK( n2n_REGISTER_ACK_t * pkt,
 int encode_REGISTER_SUPER_ACK( uint8_t * base,
                                size_t * idx,
                                const n2n_common_t * cmn,
-                               const n2n_REGISTER_SUPER_ACK_t * reg );
+                               const n2n_REGISTER_SUPER_ACK_t * reg,
+                               uint8_t * tmpbuf);
 
 int decode_REGISTER_SUPER_ACK( n2n_REGISTER_SUPER_ACK_t * reg,
                                const n2n_common_t * cmn, /* info on how to interpret it */
                                const uint8_t * base,
                                size_t * rem,
-                               size_t * idx );
+                               size_t * idx,
+                               uint8_t * tmpbuf);
 
 int fill_sockaddr( struct sockaddr * addr,
                    size_t addrlen,

src/edge_utils.c
@@ -37,7 +37,6 @@ static void check_peer_registration_needed(n2n_edge_t *eee,
 static int edge_init_sockets(n2n_edge_t *eee, int udp_local_port, int mgmt_port, uint8_t tos);
 static int edge_init_routes(n2n_edge_t *eee, n2n_route_t *routes, uint16_t num_routes);
 static void edge_cleanup_routes(n2n_edge_t *eee);
-static int supernode2addr(n2n_sock_t * sn, const n2n_sn_name_t addrIn);
 
 static void check_known_peer_sock_change(n2n_edge_t *eee,
                                          uint8_t from_supernode,
@@ -211,7 +210,7 @@ n2n_edge_t* edge_init(const n2n_edge_conf_t *conf, int *rv) {
     traceEvent(TRACE_NORMAL, "supernode %u => %s\n", i, (eee->conf.sn_ip_array[i]));
 
   /* Set the active supernode */
-  supernode2addr(&(eee->supernode), eee->conf.sn_ip_array[eee->sn_idx]);
+  supernode2sock(&(eee->supernode), eee->conf.sn_ip_array[eee->sn_idx]);
 
   /* Set active transop */
   switch(transop_id) {
@@ -318,72 +317,6 @@ static int is_valid_peer_sock(const n2n_sock_t *sock) {
 }
 
 /* ***************************************************** */
-
-/** Resolve the supernode IP address.
- *
- *  REVISIT: This is a really bad idea. The edge will block completely while the
- *           hostname resolution is performed. This could take 15 seconds.
- */
-static int supernode2addr(n2n_sock_t * sn, const n2n_sn_name_t addrIn) {
-  n2n_sn_name_t addr;
-  const char *supernode_host;
-  int rv = 0;
-
-  memcpy(addr, addrIn, N2N_EDGE_SN_HOST_SIZE);
-
-  supernode_host = strtok(addr, ":");
-
-  if(supernode_host) {
-    in_addr_t sn_addr;
-    char *supernode_port = strtok(NULL, ":");
-    const struct addrinfo aihints = {0, PF_INET, 0, 0, 0, NULL, NULL, NULL};
-    struct addrinfo * ainfo = NULL;
-    int nameerr;
-
-    if(supernode_port)
-      sn->port = atoi(supernode_port);
-    else
-      traceEvent(TRACE_WARNING, "Bad supernode parameter (-l <host:port>) %s %s:%s",
-		 addr, supernode_host, supernode_port);
-
-    nameerr = getaddrinfo(supernode_host, NULL, &aihints, &ainfo);
-
-    if(0 == nameerr)
-      {
-	struct sockaddr_in * saddr;
-
-	/* ainfo s the head of a linked list if non-NULL. */
-	if(ainfo && (PF_INET == ainfo->ai_family))
-	  {
-	    /* It is definitely and IPv4 address -> sockaddr_in */
-	    saddr = (struct sockaddr_in *)ainfo->ai_addr;
-
-	    memcpy(sn->addr.v4, &(saddr->sin_addr.s_addr), IPV4_SIZE);
-	    sn->family=AF_INET;
-	  }
-	else
-	  {
-	    /* Should only return IPv4 addresses due to aihints. */
-	    traceEvent(TRACE_WARNING, "Failed to resolve supernode IPv4 address for %s", supernode_host);
-	    rv = -1;
-	  }
-
-	freeaddrinfo(ainfo); /* free everything allocated by getaddrinfo(). */
-	ainfo = NULL;
-      } else {
-      traceEvent(TRACE_WARNING, "Failed to resolve supernode host %s", supernode_host);
-      rv = -2;
-    }
-
-  } else {
-    traceEvent(TRACE_WARNING, "Wrong supernode parameter (-l <host:port>)");
-    rv = -3;
-  }
-
-  return(rv);
-}
-
-/* ************************************** */
 
 static const int definitely_from_supernode = 1;
 
@@ -961,7 +894,7 @@ void update_supernode_reg(n2n_edge_t * eee, time_t nowTime) {
     --(eee->sup_attempts);
 
   for(sn_idx=0; sn_idx<eee->conf.sn_num; sn_idx++) {
-    if(supernode2addr(&(eee->supernode), eee->conf.sn_ip_array[sn_idx]) == 0) {
+    if(supernode2sock(&(eee->supernode), eee->conf.sn_ip_array[sn_idx]) == 0) {
       traceEvent(TRACE_INFO, "Registering with supernode [id: %u/%u][%s][attempts left %u]",
 		 sn_idx+1, eee->conf.sn_num,
 		 supernode_ip(eee), (unsigned int)eee->sup_attempts);
@@ -1901,6 +1834,7 @@ void readFromIPSocket(n2n_edge_t * eee, int in_sock) {
 	in_addr_t net;
 	char * ip_str = NULL;
 	n2n_REGISTER_SUPER_ACK_t ra;
+  uint8_t tmpbuf[MAX_AVAILABLE_SPACE_FOR_ENTRIES];
 
 	memset(&ra, 0, sizeof(n2n_REGISTER_SUPER_ACK_t));
 
@@ -1915,7 +1849,7 @@ void readFromIPSocket(n2n_edge_t * eee, int in_sock) {
 
 	if(eee->sn_wait)
 	  {
-	    decode_REGISTER_SUPER_ACK(&ra, &cmn, udp_buf, &rem, &idx);
+	    decode_REGISTER_SUPER_ACK(&ra, &cmn, udp_buf, &rem, &idx, tmpbuf);
 
 	    if(eee->conf.header_encryption == HEADER_ENCRYPTION_ENABLED) {
 	      if(!find_peer_time_stamp_and_verify (eee, definitely_from_supernode, null_mac, stamp, TIME_STAMP_NO_JITTER)) {
@@ -1940,13 +1874,10 @@ void readFromIPSocket(n2n_edge_t * eee, int in_sock) {
 
 	    if(0 == memcmp(ra.cookie, eee->last_cookie, N2N_COOKIE_SIZE))
 	      {
-#if 0
-		if(ra.num_sn > 0)  /* FIX fcarli3 */
+		if(ra.num_sn > 0)
 		  {
-		    traceEvent(TRACE_NORMAL, "Rx REGISTER_SUPER_ACK backup supernode at %s",
-			       sock_to_cstr(sockbuf1, &(ra.sn_bak)));
+		    traceEvent(TRACE_NORMAL, "Rx REGISTER_SUPER_ACK payload contains sockets and MACs of supernodes in the federation.");
 		  }
-#endif
 		eee->last_sup = now;
 		eee->sn_wait=0;
 		eee->sup_attempts = N2N_EDGE_SUP_ATTEMPTS; /* refresh because we got a response */
@@ -2548,7 +2479,7 @@ static int edge_init_routes_linux(n2n_edge_t *eee, n2n_route_t *routes, uint16_t
 	return(-1);
       }
 
-      if (supernode2addr(&sn, eee->conf.sn_ip_array[0]) < 0)
+      if (supernode2sock(&sn, eee->conf.sn_ip_array[0]) < 0)
 	return(-1);
 
       if (sn.family != AF_INET) {

src/n2n.c
@@ -221,6 +221,72 @@ char * macaddr_str(macstr_t buf,
 
 /* *********************************************** */
 
+/** Resolve the supernode IP address.
+ *
+ *  REVISIT: This is a really bad idea. The edge will block completely while the
+ *           hostname resolution is performed. This could take 15 seconds.
+ */
+int supernode2sock(n2n_sock_t * sn, const n2n_sn_name_t addrIn) {
+  n2n_sn_name_t addr;
+  const char *supernode_host;
+  int rv = 0;
+
+  memcpy(addr, addrIn, N2N_EDGE_SN_HOST_SIZE);
+
+  supernode_host = strtok(addr, ":");
+
+  if(supernode_host) {
+    in_addr_t sn_addr;
+    char *supernode_port = strtok(NULL, ":");
+    const struct addrinfo aihints = {0, PF_INET, 0, 0, 0, NULL, NULL, NULL};
+    struct addrinfo * ainfo = NULL;
+    int nameerr;
+
+    if(supernode_port)
+      sn->port = atoi(supernode_port);
+    else
+      traceEvent(TRACE_WARNING, "Bad supernode parameter (-l <host:port>) %s %s:%s",
+		 addr, supernode_host, supernode_port);
+
+    nameerr = getaddrinfo(supernode_host, NULL, &aihints, &ainfo);
+
+    if(0 == nameerr)
+      {
+	struct sockaddr_in * saddr;
+
+	/* ainfo s the head of a linked list if non-NULL. */
+	if(ainfo && (PF_INET == ainfo->ai_family))
+	  {
+	    /* It is definitely and IPv4 address -> sockaddr_in */
+	    saddr = (struct sockaddr_in *)ainfo->ai_addr;
+
+	    memcpy(sn->addr.v4, &(saddr->sin_addr.s_addr), IPV4_SIZE);
+	    sn->family=AF_INET;
+	  }
+	else
+	  {
+	    /* Should only return IPv4 addresses due to aihints. */
+	    traceEvent(TRACE_WARNING, "Failed to resolve supernode IPv4 address for %s", supernode_host);
+	    rv = -1;
+	  }
+
+	freeaddrinfo(ainfo); /* free everything allocated by getaddrinfo(). */
+	ainfo = NULL;
+      } else {
+      traceEvent(TRACE_WARNING, "Failed to resolve supernode host %s", supernode_host);
+      rv = -2;
+    }
+
+  } else {
+    traceEvent(TRACE_WARNING, "Wrong supernode parameter (-l <host:port>)");
+    rv = -3;
+  }
+
+  return(rv);
+}
+
+/* ************************************** */
+
 uint8_t is_multi_broadcast(const uint8_t * dest_mac) {
 
   int is_broadcast =(memcmp(broadcast_addr, dest_mac, 6) == 0);
@@ -278,7 +344,7 @@ void print_n2n_version() {
          GIT_RELEASE, PACKAGE_OSNAME, PACKAGE_BUILDDATE);
 }
 
-/* *********************************************** */ 
+/* *********************************************** */
 
 size_t purge_expired_registrations(struct peer_info ** peer_list, time_t* p_last_purge, int timeout) {
   time_t now = time(NULL);
@@ -373,7 +439,7 @@ extern char * sock_to_cstr(n2n_sock_str_t out,
   } else {
     const uint8_t * a = sock->addr.v4;
 
-    snprintf(out, N2N_SOCKBUF_SIZE, "%hu.%hu.%hu.%hu:%hu", 
+    snprintf(out, N2N_SOCKBUF_SIZE, "%hu.%hu.%hu.%hu:%hu",
 	     (unsigned short)(a[0] & 0xff),
 	     (unsigned short)(a[1] & 0xff),
 	     (unsigned short)(a[2] & 0xff),

src/sn.c
@@ -234,7 +234,7 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
     n2n_sock_t *socket;
     struct peer_info *anchor_sn;
     size_t length;
-    int rv;
+    int rv = -1;
 
     length = strlen(_optarg);
     if(length >= N2N_EDGE_SN_HOST_SIZE) {
@@ -251,13 +251,8 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
         anchor_sn->ip_addr = calloc(1,N2N_EDGE_SN_HOST_SIZE);
         if(anchor_sn->ip_addr){
           strncpy(anchor_sn->ip_addr,_optarg,N2N_EDGE_SN_HOST_SIZE-1);
+	        rv = supernode2sock(socket,_optarg);
 
-#if 1
-	  rv = -1;
-#else
-	  rv = supernode2sock(socket,_optarg); /* FIX fcarli3 */
-#endif
-	  
           if(rv != 0){
             traceEvent(TRACE_WARNING, "Invalid socket");
             break;
@@ -270,7 +265,7 @@ static int setOption(int optkey, char *_optarg, n2n_sn_t *sss) {
         }
       }
     }
-    
+
     break;
   }
 

src/sn_utils.c
@@ -1185,8 +1185,7 @@ static int process_udp(n2n_sn_t * sss,
 	  update_edge(sss, &reg, comm, &(ack.sock), now);
 	}
 
-	encode_REGISTER_SUPER_ACK(ackbuf, &encx, &cmn2, &ack);
-	encode_buf(ackbuf,&encx,tmpbuf,(num*ENTRY_SIZE));
+	encode_REGISTER_SUPER_ACK(ackbuf, &encx, &cmn2, &ack, tmpbuf);
 
 	if (comm->header_encryption == HEADER_ENCRYPTION_ENABLED)
 	  packet_header_encrypt (ackbuf, encx, comm->header_encryption_ctx,
@@ -1219,6 +1218,7 @@ static int process_udp(n2n_sn_t * sss,
     n2n_sock_t											*tmp_sock;
     n2n_mac_t												*tmp_mac;
     int															i;
+		uint8_t													dec_tmpbuf[MAX_AVAILABLE_SPACE_FOR_ENTRIES];
 
     sender.family = AF_INET;
     sender.port = ntohs(sender_sock->sin_port);
@@ -1237,7 +1237,7 @@ static int process_udp(n2n_sn_t * sss,
       return -1;
     }
 
-    decode_REGISTER_SUPER_ACK(&ack,&cmn,udp_buf,&rem,&idx);
+    decode_REGISTER_SUPER_ACK(&ack,&cmn,udp_buf,&rem,&idx,dec_tmpbuf);
     orig_sender = &(ack.sock);
 
     if (comm) {

src/wire.c
@@ -395,7 +395,8 @@ int decode_REGISTER_ACK(n2n_REGISTER_ACK_t *reg,
 int encode_REGISTER_SUPER_ACK(uint8_t *base,
                               size_t *idx,
                               const n2n_common_t *common,
-                              const n2n_REGISTER_SUPER_ACK_t *reg) {
+                              const n2n_REGISTER_SUPER_ACK_t *reg,
+                              uint8_t *tmpbuf) {
   int retval = 0;
   retval += encode_common(base, idx, common);
   retval += encode_buf(base, idx, reg->cookie, N2N_COOKIE_SIZE);
@@ -405,15 +406,8 @@ int encode_REGISTER_SUPER_ACK(uint8_t *base,
   retval += encode_uint16(base, idx, reg->lifetime);
   retval += encode_sock(base, idx, &(reg->sock));
   retval += encode_uint8(base, idx, reg->num_sn);
+  retval += encode_buf(base, idx, tmpbuf, (reg->num_sn*ENTRY_SIZE));
 
-#if 0 /* FIX fcarli3 */
-  if (reg->num_sn > 0) {
-    /* We only support 0 or 1 at this stage */
-    retval += encode_sock(base, idx, &(reg->sn_bak));
-    retval += encode_mac(base, idx, reg->mac_addr);
-  }
-#endif
-  
   return retval;
 }
 
@@ -422,7 +416,8 @@ int decode_REGISTER_SUPER_ACK(n2n_REGISTER_SUPER_ACK_t *reg,
                               const n2n_common_t *cmn, /* info on how to interpret it */
                               const uint8_t *base,
                               size_t *rem,
-                              size_t *idx) {
+                              size_t *idx,
+                              uint8_t *tmpbuf) {
   size_t retval = 0;
 
   memset(reg, 0, sizeof(n2n_REGISTER_SUPER_ACK_t));
@@ -437,15 +432,8 @@ int decode_REGISTER_SUPER_ACK(n2n_REGISTER_SUPER_ACK_t *reg,
 
   /* Following the edge socket are an array of backup supernodes. */
   retval += decode_uint8(&(reg->num_sn), base, rem, idx);
+  retval += decode_buf(tmpbuf, (reg->num_sn*ENTRY_SIZE), base, rem, idx);
 
-#if 0  /* FIX fcarli3 */
-  if (reg->num_sn > 0) {
-    /* We only support 0 or 1 at this stage */
-    retval += decode_sock(&(reg->sn_bak), base, rem, idx);
-    retval += decode_mac(reg->mac_addr, base, rem, idx); 
- }
-#endif
-  
   return retval;
 }
 