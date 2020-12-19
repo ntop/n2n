de92fab878757842cb5a1c22f57fde7e4de87156
Francesco Carli <62562180+fcarli3@users.noreply.github.com>
2020-12-19 12:29:09 +0100

    readability code clean-up (#549)

--- include/network_traffic_filter.h
+++ include/network_traffic_filter.h
@@ -25,13 +25,13 @@
 
 #include "n2n_typedefs.h"
 
-network_traffic_filter_t* create_network_traffic_filter();
+network_traffic_filter_t* create_network_traffic_filter ();
 
-void destroy_network_traffic_filter(network_traffic_filter_t* filter);
+void destroy_network_traffic_filter (network_traffic_filter_t* filter);
 
-void network_traffic_filter_add_rule(network_traffic_filter_t* filter, filter_rule_t* rules);
+void network_traffic_filter_add_rule (network_traffic_filter_t* filter, filter_rule_t* rules);
 
 //rule_str format: src_ip/len:[b_port,e_port],dst_ip/len:[s_port,e_port],TCP+/-,UDP+/-,ICMP+/-
-uint8_t process_traffic_filter_rule_str(const char* rule_str, filter_rule_t* rule_struct);
+uint8_t process_traffic_filter_rule_str (const char* rule_str, filter_rule_t* rule_struct);
 
 #endif //N2N_NETWORK_TRAFFIC_FILTER_H
