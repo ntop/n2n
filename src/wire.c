aa1f4311f9b519b68cd0a401f76cfecdf0ba7e08
Logan oos Even <46396513+Logan007@users.noreply.github.com>
2021-03-16 17:24:59 +0545

    adapted packet format (#662)
    * adapted packet format

--- src/wire.c
+++ src/wire.c
@@ -478,7 +478,7 @@ int encode_REGISTER_SUPER_ACK (uint8_t *base,
 
     retval += encode_common(base, idx, common);
     retval += encode_buf(base, idx, reg->cookie, N2N_COOKIE_SIZE);
-    retval += encode_mac(base, idx, reg->edgeMac);
+    retval += encode_mac(base, idx, reg->srcMac);
     retval += encode_uint32(base, idx, reg->dev_addr.net_addr);
     retval += encode_uint8(base, idx, reg->dev_addr.net_bitlen);
     retval += encode_uint16(base, idx, reg->lifetime);
@@ -501,7 +501,7 @@ int decode_REGISTER_SUPER_ACK (n2n_REGISTER_SUPER_ACK_t *reg,
     memset(reg, 0, sizeof(n2n_REGISTER_SUPER_ACK_t));
 
     retval += decode_buf(reg->cookie, N2N_COOKIE_SIZE, base, rem, idx);
-    retval += decode_mac(reg->edgeMac, base, rem, idx);
+    retval += decode_mac(reg->srcMac, base, rem, idx);
     retval += decode_uint32(&(reg->dev_addr.net_addr), base, rem, idx);
     retval += decode_uint8(&(reg->dev_addr.net_bitlen), base, rem, idx);
     retval += decode_uint16(&(reg->lifetime), base, rem, idx);
