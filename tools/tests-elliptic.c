073b2192fe4f09f2f2b8d632d78ff08150135308
Hamish Coleman <hamish@zot.org>
2021-09-16 17:38:46 +0100

    Add tests for the elliptic curve and pearson hash

---
+++ tools/tests-elliptic.c
@@ -0,0 +1,56 @@
+/*
+ * (C) 2007-21 - ntop.org and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+#include "n2n.h"
+#include "hexdump.h"
+
+void test_curve25519(unsigned char *pkt_input, unsigned char *key) {
+    char *test_name = "curve25519";
+    unsigned char pkt_output[32];
+
+    curve25519(pkt_output, key, pkt_input);
+
+    printf("%s: output\n", test_name);
+    fhexdump(0, pkt_output, sizeof(pkt_output), stdout);
+
+    fprintf(stderr, "%s: tested\n", test_name);
+    printf("\n");
+}
+
+int main(int argc, char * argv[]) {
+    char *test_name = "environment";
+
+    unsigned char key[32];
+    unsigned char pkt_input[32];
+
+    memset(pkt_input, 0, 31);
+    pkt_input[31] = 9;
+    
+    memset(key, 0x55, 32);
+
+    printf("%s: input\n", test_name);
+    fhexdump(0, pkt_input, sizeof(pkt_input), stdout);
+    printf("%s: key\n", test_name);
+    fhexdump(0, key, sizeof(key), stdout);
+    printf("\n");
+
+    test_curve25519(pkt_input, key);
+
+    return 0;
+}
+
---
+++ tools/tests-hashing.c
@@ -0,0 +1,64 @@
+/*
+ * (C) 2007-21 - ntop.org and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+#include "n2n.h"
+#include "hexdump.h"
+
+
+uint8_t PKT_CONTENT[]={
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+
+void test_pearson(void *buf, unsigned int bufsize) {
+    char *test_name = "pearson";
+
+    uint64_t hash = pearson_hash_64(buf, bufsize);
+
+    printf("%s: output = 0x%lx\n", test_name, hash);
+
+    fprintf(stderr, "%s: tested\n", test_name);
+    printf("\n");
+}
+
+int main(int argc, char * argv[]) {
+    pearson_hash_init();
+
+    char *test_name = "environment";
+    printf("%s: input size = 0x%lx\n", test_name, sizeof(PKT_CONTENT));
+    fhexdump(0, PKT_CONTENT, sizeof(PKT_CONTENT), stdout);
+    printf("\n");
+
+    test_pearson(PKT_CONTENT, sizeof(PKT_CONTENT));
+
+    return 0;
+}
+
