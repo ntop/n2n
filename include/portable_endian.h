2d3454530487428941ae3036f12a01f0e089d7cb
Logan oos Even <46396513+Logan007@users.noreply.github.com>
2021-09-30 12:39:31 +0545

    compile fix (#834)

--- include/portable_endian.h
+++ include/portable_endian.h
@@ -135,8 +135,8 @@
 #       define be32toh(x) _byteswap_ulong(x)
 #       define le32toh(x) (x)
 
-#       define htobe64(x) (((uint64_t)htonl(((uint32_t)(((uint64_t)(x)) >> 32))) & 0x00000000FFFFFFFFULL) | (((uint64_t)htonl(((uint32_t)(x)))) << 32))
-#       define be64toh(x) (((uint64_t)htonl(((uint32_t)(((uint64_t)(x)) >> 32))) & 0x00000000FFFFFFFFULL) | (((uint64_t)htonl(((uint32_t)(x)))) << 32))
+#       define htobe64(x) (((uint64_t)htobe32(((uint32_t)(((uint64_t)(x)) >> 32))) & 0x00000000FFFFFFFFULL) | (((uint64_t)htobe32(((uint32_t)(x)))) << 32))
+#       define be64toh(x) (((uint64_t)be32toh(((uint32_t)(((uint64_t)(x)) >> 32))) & 0x00000000FFFFFFFFULL) | (((uint64_t)be32toh(((uint32_t)(x)))) << 32))
 #       define htole64(x) (x)
 #       define le64toh(x) (x)
 
