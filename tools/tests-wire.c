89f557caeb10ebf47c89fab369b953b4cadec693
Hamish Coleman <hamish@zot.org>
2021-10-06 13:14:07 +0100

    Use the normal mac address format, like a boss

--- tools/tests-wire.c
+++ tools/tests-wire.c
@@ -45,7 +45,7 @@ void init_mac(n2n_mac_t mac, const uint8_t o0, const uint8_t o1,
 }
 
 void print_mac(char *test_name, char *field, n2n_mac_t mac) {
-    printf("%s: %s[] = 0x%02x,0x%02x,0x%02x,0x%02x,0x%02x,0x%02x\n",
+    printf("%s: %s[] = %x:%x:%x:%x:%x:%x\n",
             test_name, field,
             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
