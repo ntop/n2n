7d4ff082009be3c149efa95641760c05ed934b91
Hamish Coleman <hamish@zot.org>
2021-10-11 13:59:28 +0100

    added automated binary artifacts (#849)
    * Allow an autobuilder with no access to private key material to create testable packages
    
    * Initial dpkg build - will need helpers installed to work
    
    * Start adding required dpkg helpers
    
    * Tweak package artifact names
    
    * Add a windows 'package' builder
    
    * Ensure prefix path handling deals with current directory change when descending to tools dir
    
    * The tools makefile currently only needs the SBINDIR path to install properly
    
    * Add a macos 'package' builder
    
    * Remove unused configure variables
    
    * Without commit history, some of the automatic version numbering will fail
    
    * Add an rpm builder
    
    * Need to set the env var for the rpm build before we change our working dir
    
    * Allow gpg signing to fail for generating test rpm packages
    
    * Unfortunately the rpm spec file hardcodes some path assumptions, so we need to use hacks to work around them
    
    * Return to the top dir before moving things around
    
    * A small change to make actions re-run the pipeline
    
    * Name this workflow file with a nicer looking name

--- autogen.sh
+++ autogen.sh
@@ -8,9 +8,6 @@ N2N_PATCH="0"
 N2N_VERSION_SHORT="$N2N_MAJOR.$N2N_MINOR.$N2N_PATCH"
 
 cat configure.seed | sed \
-    -e "s/@N2N_MAJOR@/$N2N_MAJOR/g" \
-    -e "s/@N2N_MINOR@/$N2N_MINOR/g" \
-    -e "s/@N2N_PATCH@/$N2N_PATCH/g" \
     -e "s/@N2N_VERSION_SHORT@/$N2N_VERSION_SHORT/g" \
     > configure.ac
 
