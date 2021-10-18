ba39b5985b0755d631d9d6217187753779d90228
悠悠是梦 <radaig@163.com>
2021-10-18 11:05:44 +0800

    Update pearson.h
    恢复到8089200之前

--- include/pearson.h
+++ include/pearson.h
@@ -2,7 +2,7 @@
  * (C) 2007-20 - ntop.org and contributors
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU General Public License as published by  
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -15,11 +15,15 @@
  * along with this program; if not see see <http://www.gnu.org/licenses/>
  *
  */
+#include <stddef.h>
+#include <stdint.h>
+#if defined (__SSSE3__) && defined (__AES__) // AES-NI & SSSE3 ----------------------------------------------------
+#include <immintrin.h>
+#endif // AES-NI & SSSE3 ------------------------------------------------------------------------------------------
 
 void pearson_hash_256 (uint8_t *out, const uint8_t *in, size_t len);
-
 void pearson_hash_128 (uint8_t *out, const uint8_t *in, size_t len);
-
+uint64_t pearson_hash_64 (const uint8_t *in, size_t len);
+uint32_t pearson_hash_32 (const uint8_t *in, size_t len);
 uint16_t pearson_hash_16 (const uint8_t *in, size_t len);
-
 void pearson_hash_init();
