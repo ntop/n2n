390bf8f2c57f78c63022cd96c2c971a93e24cf2e
Hamish Coleman <hamish@zot.org>
2021-10-23 20:50:44 +0100

    Address shellcheck concerns with hack_fakeautoconf.sh

--- scripts/hack_fakeautoconf.sh
+++ scripts/hack_fakeautoconf.sh
@@ -3,7 +3,7 @@
 # Specifically for windows, where installing autoconf looks suspiciously
 # like boiling the ocean.
 
-cat Makefile.in | sed \
+sed \
     -e "s%@N2N_VERSION_SHORT@%FIXME%g" \
     -e "s%@GIT_COMMITS@%FIXME%g" \
     -e "s%@CC@%gcc%g" \
@@ -11,11 +11,11 @@ cat Makefile.in | sed \
     -e "s%@CFLAGS@%$CFLAGS%g" \
     -e "s%@LDFLAGS@%$LDFLAGS%g" \
     -e "s%@N2N_LIBS@%$LDLIBS%g" \
-    > Makefile
+    < Makefile.in > Makefile
 
-cat tools/Makefile.in | sed \
+sed \
     -e "s%@ADDITIONAL_TOOLS@%%g" \
-    > tools/Makefile
+    < tools/Makefile.in > tools/Makefile
 
 cat <<EOF >include/config.h
 #define PACKAGE_VERSION "FIXME"
