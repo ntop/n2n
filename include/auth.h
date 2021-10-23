502280c025e0c9986e50394a1ccac313e5df8010
Hamish Coleman <hamish@zot.org>
2021-10-23 19:55:25 +0100

    Drop include/auth.h from linter - there are inconsistant results with function definitions when using the current uncrustify rules

--- include/auth.h
+++ include/auth.h
@@ -24,11 +24,11 @@
 #define AUTH_H
 
 
-int bin_to_ascii(char *out, uint8_t *in, size_t in_len);
+int bin_to_ascii (char *out, uint8_t *in, size_t in_len);
 
-int ascii_to_bin(uint8_t *out, char *in);
+int ascii_to_bin (uint8_t *out, char *in);
 
-int generate_private_key(n2n_private_public_key_t key, char *in);
+int generate_private_key (n2n_private_public_key_t key, char *in);
 
 int generate_public_key (n2n_private_public_key_t pub, n2n_private_public_key_t prv);
 
