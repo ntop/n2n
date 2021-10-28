88875834661da9ecaea4df0c03c1d7f43e900733
Logan oos Even <Logan.00sEven@gmail.com>
2021-10-28 13:38:54 +0545

    compile fixes

--- include/n2n_upnpc.h
+++ include/n2n_upnpc.h
@@ -1,9 +1,29 @@
 #ifndef _N2N_UPNPC_H_
 #define _N2N_UPNPC_H_
 
+
 #include <stdint.h>
 
-int n2n_upnp_set_port_mapping(const uint16_t port);
-void n2n_upnp_del_port_mapping(const uint16_t port);
+
+#ifdef HAVE_MINIUPNP
+// include only if actually wanted (and present)
+#include "miniwget.h"
+#include "miniupnpc.h"
+#include "upnpcommands.h"
+#include "portlistingparse.h"
+#include "upnperrors.h"
+#include "miniupnpcstrings.h"
+
+#endif // HAVE_MINIUPNP
+
+
+typedef struct IGDdatas IGdatas;
+typedef struct UPNPUrls UPNPUrls;
+
+
+int n2n_upnp_set_port_mapping (const uint16_t port);
+
+void n2n_upnp_del_port_mapping (const uint16_t port);
+
 
 #endif // _N2N_UPNPC_H_
