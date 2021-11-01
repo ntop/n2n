ff72ed8190495cbf2f96657375bf4431e32a8139
Hamish Coleman <hamish@zot.org>
2021-11-01 19:13:32 +0000

    Make all version calculations come from scripts/version.sh and remove resulting redundant substitutions

--- scripts/hack_fakeautoconf.sh
+++ scripts/hack_fakeautoconf.sh
@@ -3,11 +3,7 @@
 # Specifically for windows, where installing autoconf looks suspiciously
 # like boiling the ocean.
 
-VERSION=$(cat VERSION)
-
 sed \
-    -e "s%@N2N_VERSION_SHORT@%$VERSION%g" \
-    -e "s%@GIT_COMMITS@%FIXME%g" \
     -e "s%@CC@%gcc%g" \
     -e "s%@AR@%ar%g" \
     -e "s%@CFLAGS@%$CFLAGS%g" \
