1e4e4ddb07be8554d89ce7cef80ec58380fa8f73
Hamish Coleman <hamish@zot.org>
2022-01-22 14:13:32 +0000

    Ask them what they are smoking, then tell them to put it out

--- scripts/cmake_all.sh
+++ scripts/cmake_all.sh
@@ -19,6 +19,11 @@ rm -rf build
 cmake -E make_directory build
 cd build
 
+# Shell check wants me to use an array in this scenario.  Bourne shell
+# arrays are my line in the sand showing that a script should not be
+# written in such a horrible language.  Since it would be silly to rewrite
+# a one-page wrapper script in python, we submit that this check is wrong.
+# shellcheck disable=SC2086
 cmake $OPTS ..
 
 cmake --build . --config Release
