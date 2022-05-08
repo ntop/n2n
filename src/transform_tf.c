47768758d6841a78583922cf163590d6baf0782e
Hamish Coleman <hamish@zot.org>
2022-05-08 17:45:39 +0100

    Fix use before null check

--- src/transform_tf.c
+++ src/transform_tf.c
@@ -41,10 +41,9 @@ static int transop_deinit_tf (n2n_trans_op_t *arg) {
 
     transop_tf_t *priv = (transop_tf_t *)arg->priv;
 
-    if(priv->ctx)
-        tf_deinit(priv->ctx);
-
     if(priv)
+        if(priv->ctx)
+            tf_deinit(priv->ctx);
         free(priv);
 
     return 0;
