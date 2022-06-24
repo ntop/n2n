e8db13dbb1446619923f48533a58a32550707cb6
Logan oos Even <Logan.00sEven@gmail.com>
2022-06-24 19:08:55 +0200

    one more include addition

--- include/n2n.h
+++ include/n2n.h
@@ -136,6 +136,7 @@
 
 #ifdef WIN32
 #include <windows.h>            /* for privilege check in tools/n2n-route */
+#include <lmaccess.h>           /* for privilege check in tools/n2n-route */
 #include <winsock2.h>           /* for tcp */
 #define SHUT_RDWR   SD_BOTH     /* for tcp */
 #include "wintap.h"
