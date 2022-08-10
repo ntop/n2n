b36932fbfe483421b5a44ca404c29cdf9ce9aaa4
Logan oos Even <46396513+Logan007@users.noreply.github.com>
2022-08-10 11:06:49 +0200

    feature define

--- include/n2n_define.h
+++ include/n2n_define.h
@@ -51,7 +51,9 @@
 #define REGISTER_SUPER_INTERVAL_DFL      20 /* sec, usually UDP NAT entries in a firewall expire after 30 seconds */
 #define SWEEP_TIME                       30 /* sec, indicates the value after which we have to sort the hash list of supernodes in edges
                                              * and when we send out packets to query selection-relevant informations from supernodes. */
+#ifdef HAVE_BRIDGING_SUPPORT
 #define HOSTINFO_TIMEOUT                300 /* sec, how long after last seen will the hostinfo be deleted */
+#endif
 #define NUMBER_SN_PINGS_INITIAL          15 /* number of supernodes to concurrently ping during bootstrap and immediately afterwards */
 #define NUMBER_SN_PINGS_REGULAR           5 /* number of supernodes to concurrently ping during regular edge operation */
 
