f05c24b259a964e637173bbda061e02568b6c273
Hamish Coleman <hamish@zot.org>
2023-06-28 23:55:43 +0100

    Fix openssl support and add it to tests

--- src/aes.c
+++ src/aes.c
@@ -16,6 +16,7 @@
  *
  */
 
+#include "config.h"
 
 #include <stdint.h>  // for uint32_t, uint8_t
 #include <stdlib.h>  // for calloc, free
@@ -27,6 +28,8 @@
 
 #if defined (HAVE_OPENSSL_1_1) // openSSL 1.1 ---------------------------------------------------------------------
 
+#include <openssl/err.h>    // for ERR_print_errors
+#include <openssl/evp.h>    // for EVP_EncryptInit_ex, EVP_CIPHER_CTX_set_p...
 
 // get any erorr message out of openssl
 // taken from https://en.wikibooks.org/wiki/OpenSSL/Error_handling
