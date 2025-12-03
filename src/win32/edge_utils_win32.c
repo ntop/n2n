7fd182d0505b1c694980ee9379839749b5db697e
Thomasy <thomasysliu@gmail.com>
2025-12-03 11:50:39 +0800

    Update for MINGW windows build Add missing header stdlib.h, iphlpapi.h Force type casting for CryptGenRandom, setsockopt, strncpy ifdef for getenv proto type

--- src/win32/edge_utils_win32.c
+++ src/win32/edge_utils_win32.c
@@ -128,7 +128,7 @@ int get_best_interface_ip (n2n_edge_t * eee, dec_ip_str_t *ip_addr){
             traceEvent(TRACE_DEBUG, "IP Address:    %s\n", pAdapter->IpAddressList.IpAddress.String);
             traceEvent(TRACE_DEBUG, "IP Mask:       %s\n", pAdapter->IpAddressList.IpMask.String);
             traceEvent(TRACE_DEBUG, "Gateway:       %s\n", pAdapter->GatewayList.IpAddress.String);
-            strncpy(ip_addr, pAdapter->IpAddressList.IpAddress.String, sizeof(*ip_addr));
+            strncpy((char *)ip_addr, pAdapter->IpAddressList.IpAddress.String, sizeof(*ip_addr));
         }
     } else {
         traceEvent(TRACE_WARNING, "GetAdaptersInfo failed with error: %d\n", dwRetVal);
